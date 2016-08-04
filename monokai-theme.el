f1b5d82c898aee0972436ca8ef0bdf93ee103805
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-08-04 20:31:59 +0100

    Bump version after contributions

--- monokai-theme.el
+++ monokai-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Kelvin Smith <oneKelvinSmith@gmail.com>
 ;; URL: http://github.com/oneKelvinSmith/monokai-emacs
-;; Version: 2.0.3
+;; Version: 2.1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
