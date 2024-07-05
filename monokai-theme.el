4520487c08fbeeb88d47e3a046ca593b6a771a4d
Jared Finder <finder@google.com>
2024-07-05 10:17:35 -0700

    Add support for tab-line

--- monokai-theme.el
+++ monokai-theme.el
@@ -615,6 +615,41 @@ Also affects 'linum-mode' background."
                                                      :line-width 1
                                                      :style nil)))))
 
+   ;; tab-line
+   `(tab-line
+     ((,monokai-class (:foreground ,monokai-foreground
+                                   :background ,monokai-highlight))
+      (,monokai-256-class (:foreground ,monokai-256-foreground
+                                       :background ,monokai-256-highlight))))
+   `(tab-line-highlight
+     ((,monokai-class (:underline t))
+      (,monokai-256-class (:underline t))))
+   `(tab-line-tab
+     ((,monokai-class (:foreground ,monokai-foreground
+				   :background ,monokai-background
+				   :box (:line-width 4 :color ,monokai-background)))
+      (,monokai-256-class (:foreground ,monokai-256-foreground
+				       :background ,monokai-256-background
+				       :box (:line-width 4 :color ,monokai-background)))))
+   `(tab-line-tab-current
+     ((,monokai-class (:inherit tab-line-tab)))
+      (,monokai-256-class (:inherit tab-line-tab)))
+   `(tab-line-tab-inactive
+     ((,monokai-class (:inherit tab-line-tab
+				:foreground ,monokai-comments
+				:background ,monokai-highlight
+				:box (:line-width 4 :color ,monokai-highlight)))
+      (,monokai-256-class (:inherit tab-line-tab
+				    :foreground ,monokai-256-comments
+				    :background ,monokai-256-highlight
+				    :box (:line-width 4 :color ,monokai-256-highlight)))))
+   `(tab-line-tab-inactive-alternate
+     ((,monokai-class (:inherit tab-line-tab-inactive))
+      (,monokai-256-class (:inherit tab-line-tab-inactive))))
+   `(tab-line-tab-modified
+     ((,monokai-class (:inherit tab-line-tab))
+      (,monokai-256-class (:inherit tab-line-tab))))
+
    ;; cua
    `(cua-global-mark
      ((,monokai-class (:background ,monokai-yellow
